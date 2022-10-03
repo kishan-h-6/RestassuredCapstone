3d4091d7db77de188b145a5f031e638583f82570
Kishan H <testvagrant@TestVagrants-MacBook-Pro.local>
2022-10-03 15:52:22 +0530

    added create user

--- src/test/java/sampleTest.java
+++ src/test/java/sampleTest.java
@@ -1,4 +1,4 @@
-import org.testng.Assert;
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -16,6 +16,30 @@ public class sampleTest {
                 .then()
                     .statusCode(200)
                     .log().body();
+
+    }
+
+    @Test
+    public void shouldCreateUser(){
+        //Arrange
+        //Act
+        //Assert
+        given()
+            .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization","Bearer 7ccd399ac8b7e4a6eca70af7725f91acbe0d58e3271a91c7795a3e22ad2f8310\n")
+                .body("{\n" +
+                        "    \"id\": 3957,\n" +
+                        "    \"name\": \"kishan Desai\",\n" +
+                        "    \"email\": \"kishan@rao.info\",\n" +
+                        "    \"gender\": \"male\",\n" +
+                        "    \"status\": \"active\"\n" +
+                        "}")
+                .when()
+                    .post("https://gorest.co.in/public/v2/users")
+                .then()
+                    .log().body()
+                    .statusCode(201);
 
     }
 }
