a809601fb525f6d2d4c82292f0f9c40e5a639ad7
Kishan H <testvagrant@TestVagrants-MacBook-Pro.local>
2022-10-03 14:24:07 +0530

    first commit

---
+++ src/test/java/sampleTest.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class sampleTest {
+
+    @Test
+    public void doSomething(){
+        Assert.assertEquals(1,1);
+    }
+}
